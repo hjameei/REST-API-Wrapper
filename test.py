c1765d96774d2d6b1a913e5a46005269cbf441d0
hjameei <hadisjameei@gmail.com>
2026-04-04 09:54:06 +1100

    fixing the environment variables

--- test.py
+++ test.py
@@ -1,6 +1,13 @@
 from thecatapi.rest_adapter import RestAdapter
+import os
+
+api_key = os.getenv("API_KEY")
+
+if not api_key:
+    raise RuntimeError("API_KEY is not set")
+
 my_params = {'limit': 5}
-catapi = RestAdapter(hostname="api.thecatapi.com", api_key=env("API_KEY"))
+catapi = RestAdapter(hostname="api.thecatapi.com", api_key=api_key)
 try:
     cat_list = catapi.get(endpoint="images/search/", params=my_params)
 except Exception as e:
